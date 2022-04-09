b9684492a728173b2c82219279c33ef1869de366
Claudia Lara <claudia_lara_zamora@hotmail.com>
2022-04-08 22:22:16 -0400

    smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
+$(document).ready(function(){
+    $("a").click(function(){
+        var gato = this.hash
+        $("html, body").animate(
+           { 
+               scrollTop: $(gato).offset().top -70
+           },
+           800
+        )
+    });
+})
+
+
+
 $(window).scroll(function(){
     let nav = $("html").scrollTop();
     nav > 200 ? $("nav").css("background-color", "#0dcaf0") : $("nav").css("background-color", "transparent");
     });
+
